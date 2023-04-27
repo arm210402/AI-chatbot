ea1365732f976e9b0156dc0894976940a22e08c5
arm210402 <59504320+arm210402@users.noreply.github.com>
2023-04-27 14:04:56 +0530

    Added API inlusion in APP

--- App.js
+++ App.js
@@ -1,16 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { ImageBackground, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { ImageBackground, Alert, StyleSheet, Pressable, Text, TextInput, TouchableOpacity, View, Button, SafeAreaView } from 'react-native';
 import { MaterialIcons } from "@expo/vector-icons"
 import { GiftedChat } from 'react-native-gifted-chat';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Modal from "react-native-modal";
+
 
 export default function App() {
 
   const [messages, setMessages] = useState([])
   const [inputMessage, setInputMessage] = useState("")
   const [outputMessage, setOutputMessage] = useState("results shown here")
+  const [modalVisible, setModalVisible] = useState(true);
+  const [defKey, setdefKey] = useState("")
 
+
+  const storeData = async () => {
+    console.log(defKey)
+    try {
+      await AsyncStorage.setItem('key', defKey)
+      console.log("data stored")
+    } catch (e) {
+      Alert.alert("failed to store data")
+    }
+  }
+  const getData = async () => {
+
+    try {
+      const setdefKey = await AsyncStorage.getItem('key')
+      if (setdefKey !== null) {
+        console.log(setdefKey)
+      }
+      else {
+        Alert.alert("Key is invalid or empty")
+      }
+    } catch (e) {
+      Alert.alert("Error retriving data")
+    }
+  }
+
+
+  const handleKey = (text) => {
+    setdefKey(text)
+    console.log(text)
+  }
   const handleButtonClick = () => {
+
+
     if (inputMessage.toLocaleLowerCase().startsWith("generate image")) {
       generateImages()
     }
@@ -21,81 +58,97 @@ export default function App() {
   }
 
   const generateText = () => {
-    // console.log("btn clicked")
-    const message = {
-      _id: Math.random().toString(36).substring(7),
-      text: inputMessage,
-      createdAt: new Date(),
-      user: { _id: 1 }
+
+
+    if (defKey === null && setdefKey === null) {
+      Alert.alert("Empty or Invalid key")
     }
-    setMessages((previousMessages) =>
-      GiftedChat.append(previousMessages, [message])
-    )
-    setInputMessage("")
-    fetch("https://api.openai.com/v1/chat/completions", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": "Bearer $OPENAI_API_KEY"
-      },
-      body: JSON.stringify({
-        "messages": [{ "role": "user", "content": inputMessage }],
-        "model": "gpt-3.5-turbo",
-      })
-    }).then((responce) => responce.json()).then((data) => {
-      setOutputMessage(data.choices[0].message.content.trim())
+    else {
+      // console.log("btn clicked")
       const message = {
         _id: Math.random().toString(36).substring(7),
-        text: data.choices[0].message.content.trim(),
+        text: inputMessage,
         createdAt: new Date(),
-        user: { _id: 2, name: "Open AI" }
+        user: { _id: 1 }
       }
       setMessages((previousMessages) =>
         GiftedChat.append(previousMessages, [message])
       )
-    })
-  }
+      setInputMessage("")
 
-  const generateImages = () => {
-    // console.log("btn clicked")
-    const message = {
-      _id: Math.random().toString(36).substring(7),
-      text: inputMessage,
-      createdAt: new Date(),
-      user: { _id: 1 }
-    }
-    setMessages((previousMessages) =>
-      GiftedChat.append(previousMessages, [message])
-    )
-    setInputMessage("")
-    fetch("https://api.openai.com/v1/images/generations", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": "Bearer $OPENAI_API_KEY"
-      },
-      body: JSON.stringify({
-        "prompt": inputMessage,
-        "n": 2,
-        "size": "1024x1024"
-      })
-    }).then((responce) => responce.json()).then((data) => {
-      setOutputMessage(data.data[0].url)
-      data.data.forEach((item => {
+      fetch("https://api.openai.com/v1/chat/completions", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": "Bearer " + defKey
+        },
+        body: JSON.stringify({
+          "messages": [{ "role": "user", "content": inputMessage }],
+          "model": "gpt-3.5-turbo",
+        })
+      }).then((responce) => responce.json()).then((data) => {
+        setOutputMessage(data.choices[0].message.content.trim())
         const message = {
           _id: Math.random().toString(36).substring(7),
-          text: "Image",
+          text: data.choices[0].message.content.trim(),
           createdAt: new Date(),
-          user: { _id: 2, name: "Open AI" },
-          image: item.url
+          user: { _id: 2, name: "Open AI" }
         }
+
         setMessages((previousMessages) =>
           GiftedChat.append(previousMessages, [message])
         )
 
-      }))
+      })
+    }
+  }
 
-    })
+  const generateImages = () => {
+
+    if (defKey === null && setdefKey === null) {
+      Alert.alert("Empty or Invalid key")
+    }
+    else {
+      // console.log("btn clicked")
+      const message = {
+        _id: Math.random().toString(36).substring(7),
+        text: inputMessage,
+        createdAt: new Date(),
+        user: { _id: 1 }
+      }
+      setMessages((previousMessages) =>
+        GiftedChat.append(previousMessages, [message])
+      )
+      setInputMessage("")
+      fetch("https://api.openai.com/v1/images/generations", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": "Bearer " + defKey
+        },
+        body: JSON.stringify({
+          "prompt": inputMessage,
+          "n": 2,
+          "size": "1024x1024"
+        })
+      }).then((responce) => responce.json()).then((data) => {
+        setOutputMessage(data.data[0].url)
+        data.data.forEach((item => {
+          const message = {
+            _id: Math.random().toString(36).substring(7),
+            text: "Image",
+            createdAt: new Date(),
+            user: { _id: 2, name: "Open AI" },
+            image: item.url
+          }
+          setMessages((previousMessages) =>
+            GiftedChat.append(previousMessages, [message])
+          )
+
+        }))
+
+      })
+    }
   }
   const handleTextInput = (text) => {
     setInputMessage(text)
@@ -105,29 +158,66 @@ export default function App() {
 
 
   return (
+    <>
+      <ImageBackground source={require('./assets/peakpx2.jpg')} resizeMode="cover" style={{ flex: 1, width: "100%", height: "100%" }}>
 
-    <ImageBackground source={require('./assets/peakpx2.jpg')} resizeMode="cover" style={{ flex: 1, width: "100%", height: "100%" }}>
-      <StatusBar backgroundColor="black" />
-      <View style={{ flex: 1 }}>
-        <View style={{ flex: 1, justifyContent: "center" }}>
-          <GiftedChat messages={messages} renderInputToolbar={() => { }} user={{ _id: 1 }} minInputToolbarHeight={0} />
+        <View style={styles.centeredView}>
+          <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+              //  Alert.alert('Modal has been closed.');
+              setModalVisible(!modalVisible);
+            }}>
+            <View style={styles.centeredView}>
+              <View style={styles.modalView}>
+                <Text style={styles.modalText}>Enter OpenAI API key</Text>
+                <TextInput style={{ paddingBottom: 20, marginBottom: 30, backgroundColor: "#E0E0E0", borderRadius: 20, width: 300, justifyContent: 'center' }}
+                  placeholder='   Enter Key Here' onChangeText={handleKey} value={defKey}></TextInput>
+
+                <Pressable
+                  style={[styles.button, styles.buttonClose]}
+                  onPress={() => { setModalVisible(!modalVisible); storeData() }}>
+
+                  <Text style={styles.textStyle}>Submit</Text>
+
+                </Pressable>
+              </View>
+            </View>
+          </Modal>
+          <Pressable
+            style={[styles.button, styles.buttonOpen]}
+            onPress={() => { setModalVisible(true); }}>
+            <Text style={styles.textStyle}>KEY</Text>
+          </Pressable>
+
+
         </View>
 
-        <View style={{ flexDirection: "row" }}>
-          <View style={{ flex: 1, marginLeft: 10, marginBottom: 20, borderRadius: 10, height: 60, marginLeft: 10, marginRight: 10, borderColor: "grey", borderWidth: 1, backgroundColor: "white", justifyContent: "center", paddingLeft: 10, paddingRight: 10 }}>
-            <TextInput placeholder='Enter your question' onChangeText={handleTextInput} value={inputMessage} />
+        <View style={{ flex: 1 }}>
+          <View style={{ flex: 1, justifyContent: "center" }}>
+            <GiftedChat messages={messages} renderInputToolbar={() => { }} user={{ _id: 1 }} minInputToolbarHeight={0} />
           </View>
 
-          <TouchableOpacity onPress={handleButtonClick}>
-            <View style={{ backgroundColor: "green", borderRadius: 9999, width: 60, height: 60, justifyContent: "center", padding: 5, marginRight: 10, marginBottom: 20 }}>
-              <MaterialIcons name="send" size={30} color="white" style={{ marginLeft: 10 }} />
-            </View>
-          </TouchableOpacity>
-        </View>
 
-        <StatusBar style="auto" />
-      </View>
-    </ImageBackground>
+
+          <View style={{ flexDirection: "row" }}>
+            <View style={{ flex: 1, marginLeft: 10, marginBottom: 20, borderRadius: 10, height: 60, marginLeft: 10, marginRight: 10, borderColor: "grey", borderWidth: 1, backgroundColor: "white", justifyContent: "center", paddingLeft: 10, paddingRight: 10 }}>
+              <TextInput placeholder='Enter your question' onChangeText={handleTextInput} value={inputMessage} />
+            </View>
+
+            <TouchableOpacity onPress={() => { getData(); handleButtonClick() }}>
+              <View style={{ backgroundColor: "green", borderRadius: 9999, width: 60, height: 60, justifyContent: "center", padding: 5, marginRight: 10, marginBottom: 20 }}>
+                <MaterialIcons name="send" size={30} color="white" style={{ marginLeft: 10 }} />
+              </View>
+            </TouchableOpacity>
+          </View>
+
+          <StatusBar style="auto" />
+        </View>
+      </ImageBackground>
+    </>
 
   );
 }
@@ -139,4 +229,62 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+    marginTop: 30,
+    width: 80
+  },
+  buttonOpen: {
+    backgroundColor: '#F194FF',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
 });
+
+
+{/* <View style={{ padding: 30 }}>
+        <TouchableOpacity onPress={() => setApiKeySaved(false)}>
+          <View style={{ backgroundColor: "red", borderRadius: 9999, width: 60, height: 60, justifyContent: "center", padding: 5, marginRight: 10, marginBottom: 20 }}>
+            <Text>Enter API Key</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+      {!apiKeySaved && (
+        <View>
+          <TextInput
+            placeholder="Enter API key"
+            value={apiKey}
+            onChangeText={setApiKey}
+          />
+          <TouchableOpacity onPress={saveApiKey}>
+            <Text>Save</Text>
+          </TouchableOpacity>
+        </View>
+      )} */}
